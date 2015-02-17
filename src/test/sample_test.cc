099693fbd47d977a6fc99bd1ac05dbfe4871316e
Yipei Wang <yipeiw@bros.ml.cmu.edu>
2015-02-17 17:15:12 -0500

    sample test

--- src/test/sample_test.cc
+++ src/test/sample_test.cc
@@ -1,24 +1,60 @@
-#include "filter/sample.h"
+#include "filter/weight_sensitive_sample.h"
 #include "gtest/gtest.h"
+#include <bitset>
+#include <fstream>
 
 using namespace PS;
 
 TEST(Sample, AllBasics) {
     typedef float V;
+    int arrNum = 50;
+    SArray<V> arr(arrNum, 0.0);
+    int seed = 5323;
+    for (int i=0; i < arrNum; ++i) {
+      seed = (seed * 8253729 + 2396403) % 32767;
+      int sign = (seed % 2) >0 ? 1:-1;
+      arr[i] = (seed % 100)*sign;
+    }
 
-    SArray<V> arr({-1.0, 0.3, -0.2, 0.1, -0.35, 0.6, -0.45, 0.4, 0.33, -0.05});
-    std::vector<int> test_K = {2,4,1,6,0,10};
-    LL << "testing array:";
-    LL << arr;
+    std::ofstream datafile; datafile.open("arr.log");
+    for (int i=0; i < arrNum; ++i) {
+      datafile << arr[i] << "\n";
+    }
+    datafile.close();
 
-    for(auto& K : test_K) {
+    int testK[] = {10,20,30,40};
+
+    SArray<V> test_arr;
+    for(int i=0; i<4; i++) {
+        int K = testK[i];
 	LL << "testing K: " << K;
-        SArray<V> test_arr; test_arr.copyFrom(arr);
-        sampleThresholdCut(test_arr, 2, test_arr.size(), K);
-        LL << "\nsamped results";
-        LL << test_arr;
+        test_arr.resize(arrNum, 0);
+        LL << "test arr prepare "<<arrNum;
+	for(int i=0; i<arrNum; i++) { test_arr[i] = fabs(arr[i]); }
+        LL << "start finding ...";
+
+	V threshold1 = findKthLargest(test_arr, 0, arrNum-1, K);
+        LL << "exact Kth " << threshold1;
+        V threshold2 = ApproximateKth(test_arr, K);
+        LL << "approx Kth " << threshold2;
+        //WeightSensitiveSample(test_arr, K);
+        //LL << "\nsamped results";
+        //LL << test_arr;
     }
 }
+
+/*TEST(Round, Int16) {
+    RoundingFilter rf;
+    rf.set_bit(5);
+    LL << "double type " <<typeid(double).name();
+    std::vector<double> arr = {2.13245, 0.324, 10.32, 0.65, -0.65};
+    for(auto element : arr) {
+      for (int i=0; i<5; i++) {
+        auto ele = rf.randomizedRound(element);
+        LL << ele <<"; " << std::bitset<16>(ele) << " size " <<sizeof(ele);
+      }
+    }
+}*/
 
 int main(int argc, char *argv[]) {
   FLAGS_logtostderr = 1;
